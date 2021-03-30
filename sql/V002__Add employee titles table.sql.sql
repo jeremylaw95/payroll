4b33ef8922e4f4b2d0d8e6d3a5d9e8e78db3b84b
Law <Jeremy.Law@oag.com>
2021-03-30 15:17:46 +0100

    titles and relationship between titles and emplpoyees added

---
+++ sql/V002__Add employee titles table.sql.sql
@@ -0,0 +1,11 @@
+CREATE TABLE titles (
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(40) NOT NULL,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+INSERT INTO titles (title) VALUES ("Underling");
+
+INSERT INTO titles (title) VALUES ("Evil Overlord");
+
+INSERT INTO titles (title) VALUES ("Benevolent Dictator");
