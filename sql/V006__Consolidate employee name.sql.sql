3d5a343c060e20291e5cc78b625407b87c054a05
Law <Jeremy.Law@oag.com>
2021-03-31 11:43:44 +0100

    modified migration V006

--- sql/V006__Consolidate employee name.sql.sql
+++ sql/V006__Consolidate employee name.sql.sql
@@ -8,3 +8,10 @@ DROP COLUMN last_name,
 DROP COLUMN first_name;
 
 
+ALTER VIEW employee_positions AS
+SELECT employees.id AS employee_id,
+		name,
+		title
+FROM employees
+	LEFT JOIN titles on employees.title_id = titles.id;
+	
